55cc36e9e0f7e2737d5c45ec7c8ec882e69afe66
cos-0000 <co.salomeo@gmail.com>
2024-07-16 10:54:00 +0800

    -mUpdated dashboard with sidebar

--- client/app/dashboard/page.js
+++ client/app/dashboard/page.js
@@ -6,12 +6,13 @@
 import Navbar from '../components/Navbar';
 import Total from '../components/Total';
 import CurrentDate from '../components/CurrentDate';
+import Sidebar from '../components/Sidebar';
 
 const Dashboard = () => {
 	return (
 		<>
 			<Navbar />
-
+			<Sidebar />
 			<div className='flex justify-between items-center p-4'>
 				<Total /> <CurrentDate />
 			</div>
