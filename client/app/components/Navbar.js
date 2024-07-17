809f0a31b8b7948a2bfc033c783d0c845c720a7d
cos-0000 <co.salomeo@gmail.com>
2024-07-17 16:28:10 +0800

    -mAdded sticky top in navbar

--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
 
 	if (!user) return null;
 	return (
-		<nav className='flex justify-between items-center p-3 bg-dark'>
+		<nav className='flex justify-between items-center p-3 bg-dark sticky top-0'>
 			<h1 className='flex-grow text-center justify-center items-center text-pink-50 f-heading'>
 				Family Savings
 			</h1>
