93c49ff8516aa675b8c17f87678cbdc1af414494
John Lery Cruz <cruzjohnlery1@gmail.com>
2024-08-19 15:15:49 +0800

    total components fix and added name to the total per member deposit

--- client/app/components/Total.js
+++ client/app/components/Total.js
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Total = () => {
-	const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    const fetchTotalDepositAmount = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/amount/totalDepositAmount"
+        );
+        setTotal(response.data.overallTotalDepositAmount);
+        console.log(total);
+      } catch (err) {
+        console.error("Error fetching total deposit amount:", err);
+      }
+    };
 
-	useEffect(() => {
-		fetch("http://localhost:4000/transactions")
-			.then((response) => response.json())
-			.then((data) => {
-				const totalAmount = data.reduce((acc, item) => acc + item.amount, 0);
-				setTotal(totalAmount);
-			})
-			.catch((error) => {
-				console.error("Error fetching data:", error);
-			});
-	}, []);
+    fetchTotalDepositAmount();
+  }, []);
 
-	return (
-		<div className="flex f-subheading">
-			<h3 className="text-4xl">
-				Total amount:{" "}
-				<span className="text-green-500 font-semibold">
-					P{total.toLocaleString()}
-				</span>
-			</h3>
-		</div>
-	);
+  return (
+    <div className="flex f-subheading">
+      <h3 className="text-4xl">
+        Total amount:{" "}
+        <span className="text-green-500 font-semibold">
+          P{total !== null ? total.toLocaleString() : 5}
+        </span>
+      </h3>
+    </div>
+  );
 };
 
 export default Total;
