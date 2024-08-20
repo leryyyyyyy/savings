86f33c6c708a410f821a536b31717d45ada9e0cb
John Lery Cruz <cruzjohnlery1@gmail.com>
2024-08-20 10:54:29 +0800

    fix tthe getTotalMemberDeposit api

--- server/controllers/totalAmountController.js
+++ server/controllers/totalAmountController.js
@@ -1,6 +1,7 @@
 const moment = require("moment-timezone");
 const WeeklyDeposit = require("../models/weeklyDeposit");
 const Member = require("../models/Members");
+const TotalDeposits = require("../models/totalMemberDeposit");
 
 exports.getOverallTotalDeposit = async (req, res) => {
   try {
@@ -27,28 +28,24 @@ exports.getTotalMemberDeposit = async (req, res) => {
   try {
     const memberId = req.params.id;
     const currentYear = moment().tz("Asia/Manila").year();
-    const memberDeposits = await WeeklyDeposit.find({
+
+    // Fetch the total deposit amount directly from the TotalDeposit collection
+    const totalDepositRecord = await TotalDeposits.findOne({
+      memberId: memberId,
       year: currentYear,
-      "deposits.memberId": memberId,
     });
 
-    const totalDepositAmount = memberDeposits.reduce((total, week) => {
-      const weeklyTotal = week.deposits
-        .filter((deposit) => deposit.memberId.toString() === memberId)
-        .reduce((sum, deposit) => sum + deposit.depositAmount, 0);
-      return total + weeklyTotal;
-    }, 0);
-
-    const memberName =
-      memberDeposits[0]?.deposits.find(
-        (deposit) => deposit.memberId.toString() === memberId
-      )?.memberName || "Unknown Member";
+    // Handle the case where no record is found
+    const totalAmount = totalDepositRecord ? totalDepositRecord.totalAmount : 0;
+    const memberName = totalDepositRecord
+      ? totalDepositRecord.memberName
+      : "Unknown Member";
 
     res.status(200).json({
       memberId,
       memberName,
       year: currentYear,
-      totalDepositAmount,
+      totalAmount,
     });
   } catch (err) {
     console.error(
